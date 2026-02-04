a45e7e786ebb8ca1196a163dfdb0505bde30fe67
ShivaniGanatra <shivaniganatra@live.com>
2026-02-04 17:11:22 +0000

    Added hangman if player loses a life

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
+import static org.example.Hangman.hangman;
 import static org.example.UserInput.lettersGuessed;
 import static org.example.Words.*;
 
@@ -11,6 +12,8 @@ import static org.example.Words.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+
+
         UserInput user = new UserInput();
         Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +25,6 @@ public class Main {
 
         initialWord = selectARandomWord();
         hashedWord = convertWordToHash(initialWord);
-        System.out.println("remove initial word later " + initialWord);
         user.initialMessage(hashedWord);
 
         while (lives > 0 && !(initialWord.toUpperCase().equals(initialWord))) {
@@ -42,18 +44,20 @@ public class Main {
                     System.out.printf("You have entered %s before \n",letter);
                     lives -= 1;
                     results.outputMessage(hashedWord,lettersGuessed,lives);
+                    System.out.println(hangman[lives]);
 
                 } else { // letter but its incorrect
                     user.addToArrayList(letter);
                     lives -= 1;
                     results.outputMessage(hashedWord,lettersGuessed,lives);
+                    System.out.println(hangman[lives]);
                 }
             }
 
         }  if (initialWord.toUpperCase().equals(initialWord)) { // if someones won
             System.out.println("You've won");
         } else {
-            System.out.println("You've lost");
+            System.out.printf("You've lost the word was %s", initialWord.toLowerCase());
         }
 
     }
